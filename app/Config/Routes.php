beb2c83f3e8b70f9916d667324a7ad4bd48c4cb6
Andriantsoa94 <voaharyralambo@gmail.com>
2026-05-07 10:48:19 +0300

    routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Controllers\AuthController;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/login', 'AuthController::index');
+$routes->post('/login', 'AuthController::login');
+$routes->post('/logout', 'AuthController::logout');
