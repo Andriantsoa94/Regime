0af047cd5c775bb4dedd3ca754d989e870a9a829
Andriantsoa94 <voaharyralambo@gmail.com>
2026-05-09 19:35:56 +0300

    formulaire inscription

--- app/Views/auth/inscription.php
+++ app/Views/auth/inscription.php
@@ -8,7 +8,13 @@
 <body>
     <form action="/inscrire" method="post">
         <?= csrf_field() ?>
-        
+        <label for="name">Name :</label><br>
+        <input type="name" name="name" id="name"><br>
+        <label for="email">Email :</label><br>
+        <input type="email" name="email" id="email"><br>
+        <label for="pass">Password :</label><br>
+        <input type="password" name="pass" id="pass"><br>
+        <button type="submit">Join Regime</button>
     </form>
 </body>
 </html>
