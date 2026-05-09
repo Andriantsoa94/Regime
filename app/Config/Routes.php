113d3989b1272271c508fbaebac8f8947c4ba12f
IARAKOTOARISON <irina.andriantsoa24@gmail.com>
2026-05-09 20:00:08 +0300

    routes pour le profile

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,5 +11,14 @@ $routes->get('/home', 'Home::index');
 $routes->get('/login', 'AuthController::index');
 $routes->post('/login', 'AuthController::login');
 $routes->post('/logout', 'AuthController::logout');
+
+// -----------------routes ajoutees par Irina - aza misy manoratra ato aloha----------------------------------------------------------------
+
+$routes->get('/profile', 'Profile::index');
+$routes->post('/profile/update', 'Profile::update');
+
+// -----------------------------------------------------------------------------------------------------------------------
+
+
 $routes->get('/sante', 'SanteController::index', ['filter' => 'auth']);
 $routes->post('/sante', 'SanteController::save', ['filter' => 'auth']);
